6f9c8dae950909eaf69051485a7b905c618ea9eb
DimPap99 <33203177+DimPap99@users.noreply.github.com>
2018-06-18 22:02:42 +0300

    fixed input bugs

--- mybot.py
+++ mybot.py
@@ -3,88 +3,90 @@ import time
 from bs4 import BeautifulSoup
 import pygame
 
-#Functions:
+
+# Functions:
 def Get_Games():
-    #making a request to get the source code
-    games={}
+    # making a request to get the source code
+    games = {}
     source = requests.get("http://makemehost.com/games.php").text
-    soup = BeautifulSoup(source,'lxml')
-    #finding all the <td> tags.tag is a list with those tags
-    tag=soup.find_all('div')
+    soup = BeautifulSoup(source, 'lxml')
+    # finding all the <td> tags.tag is a list with those tags
+    tag = soup.find_all('div')
 
+    # Getting the makemehost div
+    Makemehost_div_tag = soup.find('div', class_='refreshMeMMH')
+    # getting all the <td> from Makemehost div
+    Makemehost_td = Makemehost_div_tag.find_all('td')
 
-    #Getting the makemehost div
-    Makemehost_div_tag=soup.find('div',class_='refreshMeMMH')
-    #getting all the <td> from Makemehost div
-    Makemehost_td=Makemehost_div_tag.find_all('td')
+    for i in range(8, len(Makemehost_td), 5):
 
-
-    for i in range(8,len(Makemehost_td),5):
-
-         #We include in the list the elements starting from 8(the games) and we
-         #add +1 (for the capacity) if they are not equal with "".The step of
-         #the loop is 5 so that we always loop over the games.
+        # We include in the list the elements starting from 8(the games) and we
+        # add +1 (for the capacity) if they are not equal with "".The step of
+        # the loop is 5 so that we always loop over the games.
         if Makemehost_td[i].text != "":
+            # print(Makemehost_td[i].text)
+            games[Makemehost_td[i].text] = Makemehost_td[i + 1].text
 
-            #print(Makemehost_td[i].text)
-            games[Makemehost_td[i].text]=Makemehost_td[i+1].text
-
-
-    #Get the games from Enterprise Gaming in the same way
-    Enterprise_Gaming_div_tag=soup.find('div',class_='refreshMeENT')
-    #make the list of all the td tags
-    Enterprise_Gaming_td=Enterprise_Gaming_div_tag.find_all('td')
-    for i in range(4,len(Enterprise_Gaming_td),3):
-        if Enterprise_Gaming_td[i].text != "" :
-            games[Enterprise_Gaming_td[i].text]= Enterprise_Gaming_td[i+1].text
-    Partners_div_tag=soup.find('div',class_='refreshMePARTNERS')
-    Partner_td=Partners_div_tag.find_all('td')
+    # Get the games from Enterprise Gaming in the same way
+    Enterprise_Gaming_div_tag = soup.find('div', class_='refreshMeENT')
+    # make the list of all the td tags
+    Enterprise_Gaming_td = Enterprise_Gaming_div_tag.find_all('td')
+    for i in range(4, len(Enterprise_Gaming_td), 3):
+        if Enterprise_Gaming_td[i].text != "":
+            games[Enterprise_Gaming_td[i].text] = Enterprise_Gaming_td[i + 1].text
+    Partners_div_tag = soup.find('div', class_='refreshMePARTNERS')
+    Partner_td = Partners_div_tag.find_all('td')
     if Partner_td[4].text != "":
-        games[Enterprise_Gaming_td[i].text]=Enterprise_Gaming_td[i+1].text
-    #games=dict(games)
+        games[Enterprise_Gaming_td[i].text] = Enterprise_Gaming_td[i + 1].text
+    # games=dict(games)
     return games
 
-#Time prints the games whenever the counter is equal to the publish time the user specified
-#or whenever counter mod the time the user specified is equal to zero
-def Timer(time_limit,counter):
 
-    if time_limit==counter or counter % time_limit == 0:
+# Time prints the games whenever the counter is equal to the publish time the user specified
+# or whenever counter mod the time the user specified is equal to zero
+def Timer(time_limit, counter):
+    if time_limit == counter or counter % time_limit == 0:
         print(Get_Games())
         Bot_sound()
         return True
     return False
-#Specifys the time for the bot to publish
+
+
+# Specifys the time for the bot to publish
 def Publish_Time():
     while True:
         try:
-            hours=int(input("Every how many hours do you want the bot to publish the maps? "))
-            minutes=int(input("Every how many minutes do you want the bot to publish the maps? "))
-            seconds=int(input("Every how many seconds do you want the bot to publish the maps? "))
+            hours = int(input("Every how many hours do you want the bot to publish the maps? "))
+            minutes = int(input("Every how many minutes do you want the bot to publish the maps? "))
+            seconds = int(input("Every how many seconds do you want the bot to publish the maps? "))
             break
         except (ValueError):
             print("You didnt give a valid value!")
-    total_time_in_seconds= hours * 3600 + minutes * 60 + seconds
+    total_time_in_seconds = hours * 3600 + minutes * 60 + seconds
     return total_time_in_seconds
+
 
 def Bot_sound():
     pygame.mixer.init()
     pygame.mixer.music.load("bot_sound.wav")
     pygame.mixer.music.play()
-def search(answer,games,W3game):
+
+
+def search(answer, games, W3game):
     if answer != "no":
 
-        success=False
+        success = False
         for everygame in games:
             if W3game in everygame:
-                print("Game found: " + str(everygame) + ": " + games[everygame]+"\a")
-                success=True
+                print("Game found: " + str(everygame) + ": " + games[everygame] + "\a")
+                success = True
                 break
-        if success==False:
-            print("Game not found!")
-        return (success,games[everygame])
+        if success == False:
+            print("Game"+"with keyword or name " + "'"+W3game+"'"+" not found!")
+        return (success, games[everygame])
 
 
-#End of Functions--------------------------------------------------------------------------------
+# End of Functions--------------------------------------------------------------------------------
 
 current_time = time.gmtime()
 current_hour = current_time.tm_hour
@@ -101,24 +103,24 @@ while True:
 
 time_to_operate = hours * 3600 + 60 * minutes
 
-#time which decides when the bot will publish
-time_limit=Publish_Time()
+# time which decides when the bot will publish
+time_limit = Publish_Time()
 while True:
     try:
-        answer=str(input("Do you want a specific game?Write yes or no."))
-        answer=answer.lower()
-        while answer!="yes" and answer !="no":
-            print("Invalid input!")
-            answer=str(input("Do you want a specific game?Write yes or no."))
-            answer=answer.lower()
+        answer = str(input("Do you want a specific game?Write yes or no "))
+        answer = answer.lower()
+        print("Your answer was: " + answer)
         break
-    except (ValueError):
+    except ValueError:
         print("Invalid input!")
+
 if answer == "yes":
-    W3game=str(input("Give a specific name of the game or a keyword."))
-for counter in range(1,time_to_operate +1):
-        print(counter)
-        x=Timer(time_limit,counter)
-        if x==True and answer == "yes" :
-            search(answer,Get_Games(), W3game)
-        time.sleep(1)
+    W3game = str(input("Give a specific name or a keyword of the game."))
+
+for counter in range(1, time_to_operate + 1):
+    print(counter)
+    x = Timer(time_limit, counter)
+    if x==True and answer == "yes":
+        search(answer,Get_Games(),W3game)
+
+    time.sleep(1)
